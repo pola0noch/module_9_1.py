b05b9ccdc21db7b96811c3e9023b3ee4bef2ede2
pola0noch <klayriddell@gmail.com>
2024-09-12 17:02:17 +0300

    исправлены ошибки, убран лишний блок try except

--- module_9_1.py
+++ module_9_1.py
@@ -4,26 +4,19 @@ def apply_all_func(int_list, *functions):
     results = {}
 
     if not isinstance(int_list, list):
-        print("Первый аргумент должен быть списком")
-        return
+        return "Первый аргумент должен быть списком"
 
-    elif not are_all_elements_numbers(int_list):
-        print("Некорректные данные, передайте список из чисел")
-        return
+    if not are_all_elements_numbers(int_list):
+        return"Некорректные данные, передайте список из чисел"
 
     for function in functions:
-        try:
-            result = function(int_list)
-            results[function.__name__] = result
-        except Exception as exc:
-            results[function.__name__] = {"error": f"Ошибка: {exc}"}
+        result = function(int_list)
+        results[function.__name__] = result
 
     return results
 
-
 def are_all_elements_numbers(int_list):
     return all(isinstance(element, (int, float)) for element in int_list)
-
 
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
