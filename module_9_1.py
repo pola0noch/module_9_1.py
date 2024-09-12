68c6e7fd013a5cd0fee89e0779c9c74a5c80b93f
pola0noch <klayriddell@gmail.com>
2024-09-12 15:21:19 +0300

    доработана логика проверки входных данных

--- module_9_1.py
+++ module_9_1.py
@@ -3,17 +3,29 @@
 def apply_all_func(int_list, *functions):
     results = {}
 
+    if not isinstance(int_list, list):
+        print("Первый аргумент должен быть списком")
+        return
+
+    elif not are_all_elements_numbers(int_list):
+        print("Некорректные данные, передайте список из чисел")
+        return
+
     for function in functions:
         try:
             result = function(int_list)
             results[function.__name__] = result
         except Exception as exc:
-            results[function.__name__] = {f"Ошибка: {exc}"}
+            results[function.__name__] = {"error": f"Ошибка: {exc}"}
 
     return results
+
 
 def are_all_elements_numbers(int_list):
     return all(isinstance(element, (int, float)) for element in int_list)
 
+
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+print(apply_all_func([6, 20, '15', 9], max, min))
+print(apply_all_func("not a list", max, min))
